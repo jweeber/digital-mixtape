d74889a3ef169ade9622cc986ed8bcdb8e6f02f1
Jessica <jessica.weeber@gmail.com>
2016-08-08 15:55:43 -0700

    Sets up attributes in user model for data store.

--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  id: DS.attr('string'),
+  name: DS.attr('string'),
+  image_url: DS.attr('string'),
+  profile_url: DS.attr('string')
 });
