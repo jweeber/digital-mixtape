c90a5f19d1c97dc2db761a002532cd036fd39eb8
Jessica <jessica.weeber@gmail.com>
2016-08-17 20:39:17 -0700

    Finishes background color feature. Color updates firebase DB as selected.

--- app/components/mixtape/sidebar-tools.js
+++ app/components/mixtape/sidebar-tools.js
@@ -1,6 +1,8 @@
 import Ember from 'ember'
 
 export default Ember.Component.extend({
+
+  store: Ember.inject.service(),
   isOpen: false,
 
   actions: {
@@ -11,6 +13,14 @@ export default Ember.Component.extend({
 
     toggleClass: function (value) {
       $('body').css('background-color', value)
+
+      var store = this.get('store')
+      var playlistId = this.get('playlist')
+      store.findRecord('mixtape', playlistId).then( (mixtape) => {
+        console.log(mixtape)
+        mixtape.set('background_color', value)
+        mixtape.save()
+      })
     }
   }
 })
