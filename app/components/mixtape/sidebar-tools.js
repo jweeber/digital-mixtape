462a3799b2fcfc03b52383d3889a2f397e482da0
Jessica <jessica.weeber@gmail.com>
2016-08-17 21:56:13 -0700

    Adds toggle properties to each menu in the sidebar to open and close the correct ones on click.

--- app/components/mixtape/sidebar-tools.js
+++ app/components/mixtape/sidebar-tools.js
@@ -3,12 +3,20 @@ import Ember from 'ember'
 export default Ember.Component.extend({
 
   store: Ember.inject.service(),
-  isOpen: false,
+  paintIsOpen: false,
+  textIsOpen: false,
 
   actions: {
 
-    toggleOpen: function (event) {
-      this.toggleProperty('isOpen')
+    togglePaint: function () {
+      this.set('textIsOpen', false)
+      this.toggleProperty('paintIsOpen')
+    },
+
+
+    toggleText: function () {
+      this.set('paintIsOpen', false)
+      this.toggleProperty('textIsOpen')
     },
 
     toggleClass: function (value) {
