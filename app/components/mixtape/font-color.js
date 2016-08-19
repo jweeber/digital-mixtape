8f57eebd015ccd793f2e8ce8cab0e9f1de0dacf3
Jessica <jessica.weeber@gmail.com>
2016-08-18 18:34:46 -0700

    Completes feature for user being able to change font color.

---
+++ app/components/mixtape/font-color.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+  store: Ember.inject.service(),
+
+  actions: {
+    fontColor: function (value) {
+      $('body').css('color', value)
+
+      var store = this.get('store')
+      var playlistId = this.get('playlist')
+      return store.findRecord('mixtape', playlistId).then( (mixtape) => {
+        mixtape.set('font_color', value)
+        return mixtape.save()
+      })
+    }
+  }
+});
--- app/components/mixtape/sidebar-tools.js
+++ app/components/mixtape/sidebar-tools.js
@@ -5,17 +5,26 @@ export default Ember.Component.extend({
   store: Ember.inject.service(),
   paintIsOpen: false,
   textIsOpen: false,
+  textColorIsOpen: false,
 
   actions: {
 
     togglePaint: function () {
       this.set('textIsOpen', false)
+      this.set('textColorIsOpen', false)
       this.toggleProperty('paintIsOpen')
     },
 
     toggleText: function () {
       this.set('paintIsOpen', false)
+      this.set('textColorIsOpen', false)
       this.toggleProperty('textIsOpen')
+    },
+
+    toggleTextColor: function () {
+      this.set('paintIsOpen', false)
+      this.set('textIsOpen', false)
+      this.toggleProperty('textColorIsOpen')
     }
   }
 })
