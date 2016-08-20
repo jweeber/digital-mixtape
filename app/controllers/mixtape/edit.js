11a58459d9acc6384c2c5c2c07b5983bd6cc4c16
Jessica <jessica.weeber@gmail.com>
2016-08-20 11:07:25 -0700

    Photos can be retrieved from firebase, filtered by playlist id, and displayed on the page.

--- app/controllers/mixtape/edit.js
+++ app/controllers/mixtape/edit.js
@@ -1,5 +1,26 @@
 import Ember from 'ember'
 
+const services = Ember.inject.service()
+
 export default Ember.Controller.extend({
+
+  store: services,
+  session: services,
+  mixtapePhotos: [],
+
+  getPhotos: Ember.computed(function () {
+    let store = this.get('store')
+
+    return store.query('image', {
+      filter: {
+        mixtapes: this.get('playlistId')
+      }
+    }).then((images) => {
+      for (let photo of images.content) {
+        this.get('mixtapePhotos').pushObject(photo)
+      }
+      return this.get('mixtapePhotos')
+    })
+  })
 
 });
