8231321e9205e388f4477fd33163901a57e56756
Jessica <jessica.weeber@gmail.com>
2016-08-30 12:41:12 -0700

    Finishes descriptions about mixtapes and app on index page.

--- config/environment.js
+++ config/environment.js
@@ -16,7 +16,7 @@ module.exports = function(environment) {
         'spotify': {
           apiKey: process.env.SPOTIFY_ID,
           redirectUri: 'http://localhost:4200/',
-          scope: 'playlist-read-private playlist-modify-private playlist-modify-public user-read-email'
+          scope: 'playlist-modify-public user-read-email'
         }
       }
     },
